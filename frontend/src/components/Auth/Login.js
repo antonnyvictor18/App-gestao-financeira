324b2216832d34771f99bd8da44c45af8b03ca52
antonnyvictor <antonny.victor.silva@gmail.com>
2023-06-02 16:49:42 -0300

    finalizando a estrutura de modelagem do frontend

---
+++ frontend/src/components/Auth/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { login } from '../services/authService';
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      // Chamar a função de login do serviço de autenticação
+      const loggedIn = await login(username, password);
+
+      if (loggedIn) {
+        // Login bem-sucedido, redirecionar para a página principal
+        // ou executar qualquer outra ação necessária
+      } else {
+        // Exibir uma mensagem de erro ou tomar alguma ação em caso de falha no login
+      }
+    } catch (error) {
+      // Lidar com erros de autenticação, como erros de conexão com o servidor
+      console.error('Erro de autenticação:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <label htmlFor="username">Usuário:</label>
+      <input
+        type="text"
+        id="username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Digite seu usuário"
+      />
+
+      <label htmlFor="password">Senha:</label>
+      <input
+        type="password"
+        id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Digite sua senha"
+      />
+
+      <button type="submit">Entrar</button>
+    </form>
+  );
+};
+
+export default Login;
